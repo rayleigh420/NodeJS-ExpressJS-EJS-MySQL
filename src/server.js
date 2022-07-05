34e940f6f7572e4b8ecf04fd4a09afce9babcf9c
Yugiobattu420 <nhatduy0409@gmail.com>
2022-07-05 21:10:17 +0700

    static file

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import configViewEngine from './configs/viewEngine'
+require('dotenv').config()
 
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080;
 
 configViewEngine(app);
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('test/test.ejs')
 })
 
 app.listen(port, () => {
