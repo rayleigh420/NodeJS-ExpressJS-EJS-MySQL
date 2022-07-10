187e537c0cb0bef76a7b32bb2472d18201b2476a
Yugiobattu420 <nhatduy0409@gmail.com>
2022-07-10 21:11:49 +0700

    complete CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -19,6 +19,25 @@ let createUser = async (req, res) => {
     return res.redirect('/')
 }
 
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId;
+    await pool.execute('delete from users where id = ?', [userId])
+    return res.redirect('/')
+}
+
+let editUser = async (req, res) => {
+    let id = req.params.id;
+    let [user] = await pool.execute('select * from users where id = ?', [id])
+    return res.render('test/update.ejs', { user: user[0] }) // because user is a array 
+}
+
+let updateUser = async (req, res) => {
+    let { firstName, lastName, email, address, id } = req.body;
+    await pool.execute('update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?', [firstName, lastName, email, address, id])
+    return res.redirect('/')
+}
+
+
 module.exports = {
-    getHomepage, getDetailPage, createUser
+    getHomepage, getDetailPage, createUser, deleteUser, editUser, updateUser
 }
--- src/route/web.js
+++ src/route/web.js
@@ -8,6 +8,9 @@ const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage)     // tham so nao se truyen vao getHomePage ma khong can co dau ()
     router.get('/detail/user/:id', homeController.getDetailPage)
     router.post('/create-user', homeController.createUser)
+    router.post('/delete-user', homeController.deleteUser)
+    router.get('/edit-user/:id', homeController.editUser)
+    router.post('/update-user', homeController.updateUser)
 
     router.get('/about', (req, res) => {
         res.send('I am rayleigh!')
